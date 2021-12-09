b7f764c959f2dc795320652611e70f8c08e116eb
Dmitry Sedov <sedov.dmitry1@gmail.com>
2021-12-09 14:49:16 +0400

    set scss some variables globally and create Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 <template>
+  <navbar/>
   <router-view/>
 </template>
+<script>
+import { defineComponent } from 'vue';
+import Navbar from '@/components/UI/Navbar.vue';
 
+export default defineComponent({
+  components: {
+    Navbar,
+  },
+});
+
+</script>
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  color: $page-background;
 }
 
 </style>
---
+++ src/components/UI/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="navbar">
+    <div @click="$router.push('/')">Закладки</div>
+    <div class="navbar__btns">
+      <div @click="$router.push('/add-bookmark')">Добавить закладку</div>
+      <div @click="$router.push('/about')" style="margin-left: 20px">О сайте</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+};
+</script>
+
+<style scoped lang="scss">
+.navbar {
+  height: 50px;
+  background: $search-text-color;
+  box-shadow: 2px 2px 4px gray;
+  display: flex;
+  align-items: center;
+  padding: 0 15px;
+}
+.navbar__btns {
+  margin-left: auto;
+}
+</style>
